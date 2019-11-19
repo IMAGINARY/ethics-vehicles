5fe19bda20046b5e1d25a7152f306bffa58d86a0
Sebastian Uribe <suribe@gmail.com>
2019-11-19 11:53:42 +0100

    Implement 'idle' car movement through streets - item 1 from prototype

--- js/ethics.js
+++ js/ethics.js
@@ -4,28 +4,72 @@ const app = new PIXI.Application({
 });
 document.body.appendChild(app.view);
 
-const CAR_SCALE = 0.25;
-const STREET_SCALE = 0.8;
-const CAR_START_POSITION = new PIXI.Point(250, 200);
-
-function setupStreet(container) {
-    const streetTexture = PIXI.Texture.from('images/street.png');
-    const street = new PIXI.Sprite(streetTexture);
-    street.scale.x = STREET_SCALE;
-    street.scale.y = STREET_SCALE;
-    street.anchor.set(0.5);
-    container.addChild(street);
+class Street {
+    constructor(start, end, carAngle, carSpeed) {
+        this.start = start;
+        this.end = end;
+        this.carAngle = carAngle;
+        this.carSpeed = carSpeed;
+    }
 }
-    
+
+const DEFAULT_SPEED = 5;
+
+const STREETS = [
+    new Street(new PIXI.Point(400, -340), new PIXI.Point(-400, -340), 270, new PIXI.Point(-DEFAULT_SPEED, 0)),
+    new Street(new PIXI.Point(-253, -400), new PIXI.Point(-253, 400), 180, new PIXI.Point(0, DEFAULT_SPEED))
+];
+
+const CAR_SCALE = 0.2;
+const STREET_SCALE = 0.8;
+
+function setupBackground(container) {
+    const backgroundTexture = PIXI.Texture.from('images/street.png');
+    const background = new PIXI.Sprite(backgroundTexture);
+    background.scale.x = STREET_SCALE;
+    background.scale.y = STREET_SCALE;
+    background.anchor.set(0.5);
+    container.addChild(background);
+}
+
 function setupCar(container) {
     const carTexture = PIXI.Texture.from('images/car.png');
     const car = new PIXI.Sprite(carTexture);
-    car.x = CAR_START_POSITION.x;
-    car.y = CAR_START_POSITION.y;
+    car.anchor.set(0.5);
     car.scale.x = CAR_SCALE;
     car.scale.y = CAR_SCALE;
-    car.anchor.set(0.5);
+    car.angle = 90;
     container.addChild(car);
+    return car;
+}
+
+function setCarInStreet(street) {
+    car.x = street.start.x;
+    car.y = street.start.y;
+    car.angle = street.carAngle;
+}
+
+function isCarOutOfScreen() {
+    return (car.x < -400 || car.x > 400 || car.y < -400 || car.y > 400);
+}
+
+function carUpdate() {
+    car.x += currentStreet.carSpeed.x * app.ticker.deltaTime;
+    car.y += currentStreet.carSpeed.y * app.ticker.deltaTime;
+    if (isCarOutOfScreen()) {
+        resetCarMovement();
+    }
+}
+
+function resetCarMovement() {
+    index = Math.floor((Math.random() * STREETS.length)); 
+    currentStreet = STREETS[index];
+    setCarInStreet(currentStreet);
+}
+
+function startCarMovement() {
+    resetCarMovement();
+    app.ticker.add(carUpdate);
 }
 
 const container = new PIXI.Container();
@@ -33,6 +77,7 @@ app.stage.addChild(container);
 container.x = app.screen.width / 2;
 container.y = app.screen.height / 2;
 
-setupStreet(container);
-setupCar(container);
+setupBackground(container);
+const car = setupCar(container);
 
+startCarMovement();
