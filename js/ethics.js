618443e835f44b484560bc067c00b3965b1f4303
Sebastian Uribe <suribe@gmail.com>
2019-11-19 10:35:49 +0100

    Show a car turning around the screen

---
+++ js/ethics.js
@@ -0,0 +1,32 @@
+
+const app = new PIXI.Application({
+    width: 800, height: 600, backgroundColor: 0x1099bb, resolution: window.devicePixelRatio || 1,
+});
+document.body.appendChild(app.view);
+
+const container = new PIXI.Container();
+
+app.stage.addChild(container);
+
+// Create a new texture
+const texture = PIXI.Texture.from('images/car.png');
+const car = new PIXI.Sprite(texture);
+car.x = 200;
+car.y = 200;
+car.anchor.set(0.5);
+container.addChild(car);
+
+// Move container to the center
+container.x = app.screen.width / 2;
+container.y = app.screen.height / 2;
+
+// Center bunny sprite in local container coordinates
+container.pivot.x = container.width / 2;
+container.pivot.y = container.height / 2;
+
+// Listen for animate update
+app.ticker.add((delta) => {
+    // rotate the container!
+    // use delta to create frame-independent transform
+    container.rotation -= 0.01 * delta;
+});
