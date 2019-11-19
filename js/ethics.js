953ac44776057e4c6330a50802bafb8d4461bb4b
Sebastian Uribe <suribe@gmail.com>
2019-11-19 14:01:45 +0100

    Add remaining lanes

--- js/ethics.js
+++ js/ethics.js
@@ -4,21 +4,54 @@ const app = new PIXI.Application({
 });
 document.body.appendChild(app.view);
 
-class Street {
-    constructor(start, end, carAngle, carSpeed) {
+class Lane {
+    constructor(start, end, driveDirection) {
         this.start = start;
         this.end = end;
+        this.driveDirection = driveDirection;
+    }
+}
+
+class DriveDirection {
+    constructor(carAngle, speedX, speedY) {
         this.carAngle = carAngle;
-        this.carSpeed = carSpeed;
+        this.carSpeed = new PIXI.Point(speedX, speedY);
     }
 }
 
 const DEFAULT_SPEED = 5;
+const OFF_SCREEN_LIMIT = 440;
+const STREET_X_OFFSET = 228;
+const STREET_Y_OFFSET = 318;
+const STREET_LANE_OFFSET = 25;
+const DRIVE_LEFT = new DriveDirection(270, -DEFAULT_SPEED);
+const DRIVE_RIGHT = new DriveDirection(90, DEFAULT_SPEED, 0);
+const DRIVE_UP = new DriveDirection(0, 0, -DEFAULT_SPEED);
+const DRIVE_DOWN = new DriveDirection(180, 0, DEFAULT_SPEED);
 
-const STREETS = [
-    new Street(new PIXI.Point(400, -340), new PIXI.Point(-400, -340), 270, new PIXI.Point(-DEFAULT_SPEED, 0)),
-    new Street(new PIXI.Point(-253, -400), new PIXI.Point(-253, 400), 180, new PIXI.Point(0, DEFAULT_SPEED))
+const LANES = [
+    createVerticalLane(STREET_X_OFFSET - STREET_LANE_OFFSET, DRIVE_UP),
+    createVerticalLane(STREET_X_OFFSET + STREET_LANE_OFFSET, DRIVE_DOWN),
+
+    createVerticalLane(-STREET_X_OFFSET - STREET_LANE_OFFSET, DRIVE_UP),
+    createVerticalLane(-STREET_X_OFFSET + STREET_LANE_OFFSET, DRIVE_DOWN),
+
+    createHorizontalLane(STREET_Y_OFFSET + STREET_LANE_OFFSET, DRIVE_LEFT),
+    createHorizontalLane(STREET_Y_OFFSET - STREET_LANE_OFFSET, DRIVE_RIGHT),
+
+    createHorizontalLane(-STREET_Y_OFFSET + STREET_LANE_OFFSET, DRIVE_LEFT),
+    createHorizontalLane(-STREET_Y_OFFSET - STREET_LANE_OFFSET, DRIVE_RIGHT),
 ];
+
+function createHorizontalLane(verticalOffset, driveDirection) {
+    dirMultiplier = driveDirection.carSpeed.x > 0 ? -1 : 1;
+    return new Lane(new PIXI.Point(OFF_SCREEN_LIMIT * dirMultiplier, -verticalOffset), new PIXI.Point(-OFF_SCREEN_LIMIT * dirMultiplier, -verticalOffset), driveDirection);
+}
+
+function createVerticalLane(horizontalOffset, driveDirection) {
+    dirMultiplier = driveDirection.carSpeed.y > 0 ? -1 : 1;
+    return new Lane(new PIXI.Point(-horizontalOffset, OFF_SCREEN_LIMIT * dirMultiplier), new PIXI.Point(horizontalOffset, -OFF_SCREEN_LIMIT * dirMultiplier), driveDirection);
+}
 
 const CAR_SCALE = 0.2;
 const STREET_SCALE = 0.8;
@@ -43,28 +76,28 @@ function setupCar(container) {
     return car;
 }
 
-function setCarInStreet(street) {
-    car.x = street.start.x;
-    car.y = street.start.y;
-    car.angle = street.carAngle;
+function setCarInLane(lane) {
+    car.x = lane.start.x;
+    car.y = lane.start.y;
+    car.angle = lane.driveDirection.carAngle;
 }
 
 function isCarOutOfScreen() {
-    return (car.x < -400 || car.x > 400 || car.y < -400 || car.y > 400);
+    return (car.x < -OFF_SCREEN_LIMIT || car.x > OFF_SCREEN_LIMIT || car.y < -OFF_SCREEN_LIMIT || car.y > OFF_SCREEN_LIMIT);
 }
 
 function carUpdate() {
-    car.x += currentStreet.carSpeed.x * app.ticker.deltaTime;
-    car.y += currentStreet.carSpeed.y * app.ticker.deltaTime;
+    car.x += currentLane.driveDirection.carSpeed.x * app.ticker.deltaTime;
+    car.y += currentLane.driveDirection.carSpeed.y * app.ticker.deltaTime;
     if (isCarOutOfScreen()) {
         resetCarMovement();
     }
 }
 
 function resetCarMovement() {
-    index = Math.floor((Math.random() * STREETS.length)); 
-    currentStreet = STREETS[index];
-    setCarInStreet(currentStreet);
+    index = Math.floor((Math.random() * LANES.length)); 
+    currentLane = LANES[index];
+    setCarInLane(currentLane);
 }
 
 function startCarMovement() {
