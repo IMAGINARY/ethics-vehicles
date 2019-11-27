545f8e0c935d388c559353f9b3f7fee800dd2efb
Sebastian Uribe <suribe@gmail.com>
2019-11-27 14:05:49 +0100

    Forcing car angle when placing it in a lane can now be skipped

--- js/movement.js
+++ js/movement.js
@@ -1,8 +1,9 @@
 
-function placeCarInLane(car, lane, position = 0.0) {
+function placeCarInLane(car, lane, position = 0.0, forceAngle = true) {
     car.x = lane.start.x + (lane.end.x - lane.start.x) * position;
     car.y = lane.start.y + (lane.end.y - lane.start.y) * position;
-    car.angle = lane.driveDirection.carAngle;
+    if (forceAngle)
+        car.angle = lane.driveDirection.carAngle;
 }
 
 function updateCar(car, lane, elapsed) {
@@ -10,9 +11,9 @@ function updateCar(car, lane, elapsed) {
     car.y += lane.driveDirection.carSpeed.y * elapsed;
 }
 
-function advanceCarThroughLane(car, lane, startPosition = 0.0, endPosition = 1.0) {
+function advanceCarThroughLane(car, lane, startPosition = 0.0, endPosition = 1.0, forceAngle = true) {
     return new Promise((resolve, reject) => {
-        placeCarInLane(car, lane, startPosition);
+        placeCarInLane(car, lane, startPosition, forceAngle);
 
         let update = () => {
             updateCar(car, lane, app.ticker.deltaTime);
