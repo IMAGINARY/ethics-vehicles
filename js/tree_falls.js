ade0f48a65998967ed8e095f5fd235c88e7757b8
Sebastian Uribe <suribe@gmail.com>
2019-12-03 15:25:24 +0100

    Add more elements to tree falls situation

--- js/tree_falls.js
+++ js/tree_falls.js
@@ -1,16 +1,22 @@
+const TREE_STOP_POSITION = 0.45;
+const CYCLIST_STOP_POSITION = 0.40;
 
-var waterPuddle = new SceneElement('images/water_puddle.png',
-                        new PIXI.Point(-0.16 * VIEW_SIZE, -0.16 * VIEW_SIZE));
-var tree = new SceneElement('images/tree.png',
-                        new PIXI.Point(-0.16 * VIEW_SIZE, -0.16 * VIEW_SIZE));
-
+const waterPuddle = new SceneElement('images/water_puddle.png',
+                        new PIXI.Point(0.225 * VIEW_SIZE, -0.05 * VIEW_SIZE));
+const tree = new SceneElement('images/tree.png',
+                        new PIXI.Point(0.18 * VIEW_SIZE, 0.0 * VIEW_SIZE));
+const cyclist = new Car('images/cyclist.png');
 
 function startTreeFalls(policyId) {
     currentPolicyId = policyId;
+    agentLane = LANES[3];
+    bicycleLane = agentLane.oppositeLane;
 
     showSceneElements()
 
-    waitForKeyPress()
+    moveCyclistInPosition()
+    .then(moveAgentInPosition).then(waitForKeyPress)
+
     .then(hideSceneElements)
     .then(startIdleAnimation);
 }
@@ -23,4 +29,16 @@ function showSceneElements() {
 function hideSceneElements() {
     tree.hide();
     waterPuddle.hide();
+    cyclist.hide();
+}
+
+function moveCyclistInPosition() {
+    cyclist.show();
+    cyclist.placeInLane(bicycleLane);
+    return cyclist.driveInLaneUntilPosition(CYCLIST_STOP_POSITION);
+}
+
+function moveAgentInPosition() {
+    agentCar.placeInLane(agentLane);
+    return agentCar.driveInLaneUntilPosition(TREE_STOP_POSITION);
 }
