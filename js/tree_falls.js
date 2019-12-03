33016a099cb01fc84128267b212453496a9c7388
Sebastian Uribe <suribe@gmail.com>
2019-12-03 14:12:19 +0100

    Add scene elements to tree falls situation

--- js/tree_falls.js
+++ js/tree_falls.js
@@ -1,5 +1,26 @@
 
-function startTreeFalls(policy) {
-    console.log('Tree falls: not implemented');
-    startIdleAnimation();
+var waterPuddle = new SceneElement('images/water_puddle.png',
+                        new PIXI.Point(-0.16 * VIEW_SIZE, -0.16 * VIEW_SIZE));
+var tree = new SceneElement('images/tree.png',
+                        new PIXI.Point(-0.16 * VIEW_SIZE, -0.16 * VIEW_SIZE));
+
+
+function startTreeFalls(policyId) {
+    currentPolicyId = policyId;
+
+    showSceneElements()
+
+    waitForKeyPress()
+    .then(hideSceneElements)
+    .then(startIdleAnimation);
+}
+
+function showSceneElements() {
+    tree.show();
+    waterPuddle.show();
+}
+
+function hideSceneElements() {
+    tree.hide();
+    waterPuddle.hide();
 }
