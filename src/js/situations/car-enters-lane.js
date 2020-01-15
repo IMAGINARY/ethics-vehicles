a98725be1d5a41bddfaf3435eb3a9bf23eb2fb93
Sebastian Uribe <suribe@gmail.com>
2020-01-15 17:38:17 +0100

    Setup of car enters lane now animated simultaneously

--- src/js/situations/car-enters-lane.js
+++ src/js/situations/car-enters-lane.js
@@ -9,8 +9,26 @@ const BUS_STOP_X = (VIEW_SIZE/2) - BORDER_BLOCK_SIZE + (SPRITE_WIDTH/2);
 const BUS_STOP_Y = -0.06 * VIEW_SIZE;
 
 const TRUCK_STOP_POSITION = 0.45;
-const BLACK_CAR_STOP_POSITION = 0.38;
+const BLACK_CAR_STOP_POSITION = 0.35;
 const AGENT_STOP_POSITION = 0.45;
+
+const BLACK_CAR_CROSS_POSITION = new PIXI.Point(STREET_LANE_OFFSET * 2, STREET_LANE_OFFSET * 2);
+
+/*
+Timeline:
+0 - 1000: truck enters and parks
+1000 - 1500: black car enters and stops behind truck
+1500 - 2500: agent enters and reaches position
+2250 - 2500: black car crosses lane
+*/
+
+const SETUP_TIME = 1500;
+const ENTER_TRUCK_TIME = 1000;
+const AGENT_ENTER_DELAY = 500;
+const BLACK_CAR_ENTRY_TIME = 500;
+const BLACK_CAR_PARKED_DELAY = 750;
+const BLACK_CAR_CROSS_TIME = 250;
+
 
 export default class CarEntersLaneSituation extends Situation {
   constructor(view) {
@@ -32,10 +50,10 @@ export default class CarEntersLaneSituation extends Situation {
   }
 
   start() {
-    return this.moveTruckInPosition()
-      .then(() => this.moveBlackCarInPosition())
-      .then(() => this.moveAgentInPosition())
-      .then(() => this.blackCarCrossesLane());
+    this.view.agentCar.hide();
+    return this.moveTruckInPosition().then(
+      () => Promise.all([this.moveBlackCarInPosition(),
+                         this.moveAgentInPosition()]));
   }
 
   getElements() {
@@ -103,22 +121,27 @@ export default class CarEntersLaneSituation extends Situation {
     this.view.agentCar.x += STREET_LANE_OFFSET * 2;
   }
 
-
   moveTruckInPosition() {
     this.addSprite(this.truck.sprite);
     this.truck.placeInLane(this.parkedLane);
-    return this.truck.driveInLaneUntilPosition(TRUCK_STOP_POSITION);
+    return this.truck.driveInLaneUntilPosition(TRUCK_STOP_POSITION, ENTER_TRUCK_TIME, TWEEN.Easing.Cubic.Out);
   }
 
   moveBlackCarInPosition() {
     this.addSprite(this.blackCar.sprite);
     this.blackCar.placeInLane(this.parkedLane);
-    return this.blackCar.driveInLaneUntilPosition(BLACK_CAR_STOP_POSITION);
+    return this.blackCar.driveInLaneUntilPosition(BLACK_CAR_STOP_POSITION, BLACK_CAR_ENTRY_TIME, TWEEN.Easing.Sinusoidal.Out)
+      .then( () => this.wait(BLACK_CAR_PARKED_DELAY))
+      .then( () => this.blackCar.advanceAndTurn(BLACK_CAR_CROSS_POSITION, -30, BLACK_CAR_CROSS_TIME, TWEEN.Easing.Quadratic.InOut));
   }
 
   moveAgentInPosition() {
-    this.view.agentCar.placeInLane(this.agentLane);
-    return this.view.agentCar.driveInLaneUntilPosition(AGENT_STOP_POSITION);
+    return this.wait(AGENT_ENTER_DELAY)
+            .then( () => {
+              this.view.agentCar.show();
+              this.view.agentCar.placeInLane(this.agentLane);
+              this.view.agentCar.driveInLaneUntilPosition(AGENT_STOP_POSITION, SETUP_TIME - AGENT_ENTER_DELAY);
+            });
   }
 
   blackCarCrossesLane() {
