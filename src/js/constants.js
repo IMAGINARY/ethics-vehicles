f206bcabab20a487bb31635b034a3e0e278256df
Sebastian Uribe <suribe@gmail.com>
2020-01-29 12:29:12 +0100

    adjust lane positions

--- src/js/constants.js
+++ src/js/constants.js
@@ -3,10 +3,12 @@ export const ViewSize = {
   width: 1920,
   height: 1080
 };
+export const StreetOffsetFromCenter = {
+  x: 560,
+  y: 350
+}
 export const DEFAULT_SPEED = 15;
 export const IDLE_ANIMATION_TIME = 2000;
-export const STREET_X_OFFSET = (ViewSize.width/2) - 176;
-export const STREET_Y_OFFSET = (ViewSize.height/2) - 176;
 export const STREET_WIDTH = 160;
 export const BLOCK_SIZE = 512;
 export const BORDER_BLOCK_SIZE = 96;
--- src/js/lanes.js
+++ src/js/lanes.js
@@ -1,34 +1,32 @@
 /* globals PIXI */
 import { Lane } from './lane';
-import {
-  STREET_X_OFFSET, STREET_Y_OFFSET, STREET_LANE_OFFSET, ViewSize,
-} from './constants';
+import { StreetOffsetFromCenter, STREET_LANE_OFFSET, ViewSize, } from './constants';
 
 function createHorizontalLane(verticalOffset, dirMultiplier) {
   return new Lane(
     new PIXI.Point( (ViewSize.width/2) * dirMultiplier, -verticalOffset),
-    new PIXI.Point(-(ViewSize.height/2) * dirMultiplier, -verticalOffset));
+    new PIXI.Point(-(ViewSize.width/2) * dirMultiplier, -verticalOffset));
 }
 
 function createVerticalLane(horizontalOffset, dirMultiplier) {
   return new Lane(
-    new PIXI.Point(-horizontalOffset,  (ViewSize.width/2) * dirMultiplier),
+    new PIXI.Point(-horizontalOffset,  (ViewSize.height/2) * dirMultiplier),
     new PIXI.Point(-horizontalOffset, -(ViewSize.height/2) * dirMultiplier));
 }
 
 // eslint-disable-next-line import/prefer-default-export
 export const LANES = [
-  createVerticalLane(STREET_X_OFFSET - STREET_LANE_OFFSET, 1),
-  createVerticalLane(STREET_X_OFFSET + STREET_LANE_OFFSET, -1),
+  createVerticalLane(StreetOffsetFromCenter.x - STREET_LANE_OFFSET, 1),
+  createVerticalLane(StreetOffsetFromCenter.x + STREET_LANE_OFFSET, -1),
 
-  createVerticalLane(-STREET_X_OFFSET - STREET_LANE_OFFSET, 1),
-  createVerticalLane(-STREET_X_OFFSET + STREET_LANE_OFFSET, -1),
+  createVerticalLane(-StreetOffsetFromCenter.x - STREET_LANE_OFFSET, 1),
+  createVerticalLane(-StreetOffsetFromCenter.x + STREET_LANE_OFFSET, -1),
 
-  createHorizontalLane(STREET_Y_OFFSET + STREET_LANE_OFFSET, 1),
-  createHorizontalLane(STREET_Y_OFFSET - STREET_LANE_OFFSET, -1),
+  createHorizontalLane(StreetOffsetFromCenter.y + STREET_LANE_OFFSET, 1),
+  createHorizontalLane(StreetOffsetFromCenter.y - STREET_LANE_OFFSET, -1),
 
-  createHorizontalLane(-STREET_Y_OFFSET + STREET_LANE_OFFSET, 1),
-  createHorizontalLane(-STREET_Y_OFFSET - STREET_LANE_OFFSET, -1),
+  createHorizontalLane(-StreetOffsetFromCenter.y + STREET_LANE_OFFSET, 1),
+  createHorizontalLane(-StreetOffsetFromCenter.y - STREET_LANE_OFFSET, -1),
 ];
 
 function setOppositeLanes(laneA, laneB) {
