83712c0b4bc34730be8b040bbcece8d22251d7a1
Sebastian Uribe <suribe@gmail.com>
2020-01-29 16:51:29 +0100

    Add BorderBlock size

--- src/js/constants.js
+++ src/js/constants.js
@@ -6,11 +6,14 @@ export const ViewSize = {
 export const StreetOffsetFromCenter = {
   x: 560,
   y: 350
-}
+};
+export const BorderBlockSize = {
+  width: 320,
+  height: 110
+};
 export const DEFAULT_SPEED = 15;
 export const IDLE_ANIMATION_TIME = 2000;
 export const STREET_WIDTH = 160;
 export const BLOCK_SIZE = 512;
-export const BORDER_BLOCK_SIZE = 96;
 export const STREET_LANE_OFFSET = STREET_WIDTH/4;
 export const SPRITE_WIDTH = 256 * CAR_SCALE;
